af8290d385b7980d90fb08ab0328e1a6c9194450
Tycoonlover1359 <6766257+tycoonlover1359@users.noreply.github.com>
2022-01-01 16:39:56 -0800

    Turned ClanBot Repo to a blank slate

--- README.md
+++
@@ -1,3 +0,0 @@
-# Roblox Library Bot
-
-The bot for this server: https://discord.gg/xPD6sPt
--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = client => {
-  client.user.setActivity(client.config.Discord_Prefix + "help", {
-    type: "PLAYING"
-  });
-  console.log("Connected to Discord as " + client.user.tag + "!");
-  console.log(" - - - - - - - - - - - - - - - - - - - - -");
-};
